6db322d19de2ca3f3a4c7d9499cf2c19bb2acc4f
Rafał Majkowski <majkipl@gmail.com>
2019-10-01 09:41:31 +0200

    initialize repository

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+chown -R www-data:www-data /var/www/html/public
+chown -R www-data:www-data /var/www/html/storage
+chmod -R 777 /var/www/html/public
+chmod -R 777 /var/www/html/storage
+
+composer install --no-interaction --no-progress
+
+npm install -g n
+n 12.13.0
+npm install
+npm run build
+
+php artisan key:generate            # generujemy klucz
+php artisan migrate:fresh --seed    # robimy migracje wraz z seed'ami #
+php artisan cache:clear             # czyścimy cache
+php artisan config:clear            # czyścimy konfiguracje
+php artisan route:clear             # czyścimy routing
+
+cd public
+ln -sf ../storage/app/public/ storage
+
+# utrzymuje kontener przy życiu - nie zamyka apache
+# wyświetla logi apacha
+exec docker-php-entrypoint apache2-foreground
---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charSet="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="{{ config('app.name', 'Laravel') }}"/>
+    <meta name="keywords" content="{{ config('app.name', 'Laravel') }}"/>
+
+    <!--Facebook Like Button OpenGraph Settings Start-->
+
+{{--    <meta property="og:type" content="website"/>--}}
+{{--    @isset($contest)--}}
+{{--        <meta property="og:url" content="{{ route('front.application.id', ['contest' => $contest->id]) }}"/>--}}
+{{--        @if($contest->img_tip)--}}
+{{--            <meta property="og:image" content="{{ asset('storage/' . $contest->img_tip) }}"/>--}}
+{{--        @endif--}}
+{{--        @if($contest->video_type == 'youtube')--}}
+{{--            <meta property="og:image" content="{{ $contest->video_image_id }}"/>--}}
+{{--        @endif--}}
+{{--        @if($contest->video_type == 'vimeo')--}}
+{{--            <meta property="og:image" content="{{ $contest->video_image_id }}"/>--}}
+{{--        @endif--}}
+{{--        <meta property="og:site_name" content="{{ $contest->title }}"/>--}}
+{{--        <meta property="og:title" content="{{ $contest->title }}"/>--}}
+{{--        <meta name="dcterms.Title" content="{{ $contest->title }}"/>--}}
+{{--        <meta name="dcterms.Subject" content="{{ $contest->title }}"/>--}}
+{{--        <meta property="og:description" content="{{ $contest->message }}"/>--}}
+{{--    @else--}}
+{{--        <meta property="og:url" content="{{ route('front.home') }}"/>--}}
+{{--        <meta property="og:image" content="{{ asset('images/kv-men.jpg') }}"/>--}}
+{{--        <meta property="og:site_name" content="{{ config('app.name', 'Laravel') }}"/>--}}
+{{--        <meta property="og:title" content="{{ config('app.name', 'Laravel') }}"/>--}}
+{{--        <meta name="dcterms.Title" content="{{ config('app.name', 'Laravel') }}"/>--}}
+{{--        <meta name="dcterms.Subject" content="{{ config('app.name', 'Laravel') }}"/>--}}
+{{--        <meta property="og:description" content="{{ config('app.name', 'Laravel') }}"/>--}}
+{{--    @endisset--}}
+{{--    <meta property="og:image:width" content="330"/>--}}
+{{--    <meta property="og:image:height" content="330"/>--}}
+{{--    <meta property="fb:app_id" content="869984166500416"/>--}}
+
+    {{--    <!--Facebook Like Button OpenGraph Settings End-->--}}
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://use.typekit.net/rnp1gho.css">
+
+    <link href="{{ asset('css/front.css') }}" rel="stylesheet">
+
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+</head>
+
+<body class="cbp-spmenu-push{{ isset($include_body_class) ? ' ' . $include_body_class : '' }}">
+
+@include('common.loader')
+@include('common.top')
+
+@yield('content')
+
+@include('common.footer')
+
+<!-- Scripts -->
+<script src="{{ asset('js/front.js') }}" defer></script>
+
+</body>
+</html>
